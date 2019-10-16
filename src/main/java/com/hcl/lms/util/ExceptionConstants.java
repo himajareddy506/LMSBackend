daa375a91ec63847b664a2624b5ad0429a17d88d
PanaromicHCL <Panaromic@hcl1>
2019-10-16 17:57:34 +0530

    Scheduler

--- src/main/java/com/hcl/lms/util/ExceptionConstants.java
+++ src/main/java/com/hcl/lms/util/ExceptionConstants.java
@@ -7,11 +7,9 @@ public class ExceptionConstants {
 	}
 
 	public static final String USER_NOT_FOUND = "Invalid credentials";
-
 	public static final String BOOK_NOT_AVAILABLE = "No Books Available";
-
-
 	public static final String EMAIL_INVALID = "Invalid emailId";
 	public static final String MOBILE_INVALID = "Invalid Mobile Number";
+	public static final String releaseStatus="released";
 
 }
