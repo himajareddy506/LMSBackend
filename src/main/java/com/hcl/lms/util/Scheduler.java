22bf6c4a3423e4046ce58e303db046bc6c539d79
PanaromicHCL <Panaromic@hcl1>
2019-10-16 17:45:47 +0530

    Added Borrow Detail

--- src/main/java/com/hcl/lms/util/Scheduler.java
+++ src/main/java/com/hcl/lms/util/Scheduler.java
@@ -1,13 +1,21 @@
 package com.hcl.lms.util;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.hcl.lms.entity.BorrowDetail;
+import com.hcl.lms.repository.BorrowDetailRepository;
+
 @Component
 public class Scheduler {
+	@Autowired
+	BorrowDetailRepository borrowDetailRepository;
 	@Scheduled(cron = "0 0/1 * * * *")
-	
 	public void bookReleaseSchedule() {
-		
+		List<BorrowDetail> borrowList=borrowDetailRepository.findByReleaseDate(LocalDate.now());
 	}
 }
