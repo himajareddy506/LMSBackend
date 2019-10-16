d669366be771eb261ebd85ad6f46f1b25319cd83
Jyoshna <moramjyoshna@gmail.com>
2019-10-16 17:55:11 +0530

    Commit

--- src/main/java/com/hcl/lms/dto/LoginRequestDto.java
+++ src/main/java/com/hcl/lms/dto/LoginRequestDto.java
@@ -2,26 +2,19 @@ package com.hcl.lms.dto;
 
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class LoginRequestDto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private String emailId;
 	private String passcode;
 	
-	public String getEmailId() {
-		return emailId;
-	}
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-	public String getPasscode() {
-		return passcode;
-	}
-	public void setPasscode(String passcode) {
-		this.passcode = passcode;
-	}
-	public LoginRequestDto() {
-		super();
-	}
+	
 	
 }
